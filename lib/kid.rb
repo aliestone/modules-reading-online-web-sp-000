17778a3bf8084f6f6ee9ef9354e5577249f040c9
Alie Stone <helloaliestone@gmail.com>
2020-10-08 19:50:22 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,9 +3,8 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-
-  include Dance
-  extend MetaDancing
+include Dance
+extend MetaDancing
  
   attr_accessor :name
 
